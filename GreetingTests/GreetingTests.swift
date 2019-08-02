47f70b0d23de03522dbe1553afbbb6d83732e580
Samuel Stoltzfus <samuel@mikesclan.com>
2019-08-02 08:32:12 -0600

    added test file

---
+++ GreetingTests/GreetingTests.swift
@@ -0,0 +1,15 @@
+//
+//  GreetingTests.swift
+//  GreetingTests
+//
+//  Created by Samuel Stoltzfus on 8/2/19.
+//  Copyright © 2019 Superior Plastic Products, Inc. All rights reserved.
+//
+
+import XCTest
+
+class GreetingTests: XCTestCase {
+
+    
+
+}

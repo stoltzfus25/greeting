f9f833ba941718c68039db85067cd132df2e6ea1
Samuel Stoltzfus <samuel@superiorplastic.net>
2019-08-02 08:25:38 -0600

    Initial Commit

---
+++ Greeting/Greeting.h
@@ -0,0 +1,19 @@
+//
+//  Greeting.h
+//  Greeting
+//
+//  Created by Samuel Stoltzfus on 8/2/19.
+//  Copyright © 2019 Superior Plastic Products, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Greeting.
+FOUNDATION_EXPORT double GreetingVersionNumber;
+
+//! Project version string for Greeting.
+FOUNDATION_EXPORT const unsigned char GreetingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Greeting/PublicHeader.h>
+
+
